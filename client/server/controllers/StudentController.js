c0aa59ae3b0ccc95a022e5e058f4290cc422884a
Ryan Griggs <griggsspace@gmail.com>
2023-06-05 16:49:45 -0700

    adding studentController.deleteStudent

--- client/server/controllers/StudentController.js
+++ client/server/controllers/StudentController.js
@@ -3,9 +3,11 @@ const classroomDB = require('../models/classroomModel');
 
 const studentController = {};
 
+// *** ALL QUERIES CURRENTLY ASSUME STUDENT TABLE TO BE LABELED 'students' and CLASSROOM TABLE TO BE LABELED 'classrooms' ***
+
 studentController.addStudent = (req, res, next) => {
-  console.log('studentController.addStudent Request Body: ', req.body);
   const { firstName, lastName, classroomID } = req.body;
+  console.log('studentController.addStudent adding:', firstName, lastName, classroomID);
   const queryText = `INSERT INTO students (firstName, lastName, classroomID) VALUES (${firstName}, ${lastName}, ${classroomID})`;
   studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
     if(err) {
@@ -14,5 +16,16 @@ studentController.addStudent = (req, res, next) => {
   });
   return next();
 };
+
+studentController.deleteStudent = (req, res, next) => {
+  const { firstName, lastName, classroomID } = req.body;
+  const queryText = `DELETE FROM students WHERE firstName = ${firstName} AND lastName = ${lastName} AND classroomID = ${classroomID}`;
+  studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
+    if(err) {
+      console.log('studentController.deleteStudent encountered an error:', err);
+    }
+  });
+  return next();
+}
 
 module.exports = studentController;
