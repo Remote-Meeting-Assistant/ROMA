098641fb473e4e5eaf7a926f51fd46bd2f6c73bc
Ryan Griggs <griggsspace@gmail.com>
2023-06-05 17:07:51 -0700

    Adding studentController.addclap

--- server/controllers/StudentController.js
+++ server/controllers/StudentController.js
@@ -8,7 +8,7 @@ const studentController = {};
 studentController.addStudent = (req, res, next) => {
   const { firstName, lastName, classroomID } = req.body;
   console.log('studentController.addStudent adding:', firstName, lastName, classroomID);
-  const queryText = `INSERT INTO students (firstName, lastName, classroomID) VALUES (${firstName}, ${lastName}, ${classroomID})`;
+  const queryText = `INSERT INTO students (first_name, last_name, classroomID, claps) VALUES (${firstName}, ${lastName}, ${classroomID}, 0)`;
   studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
     if(err) {
       console.log('studentController.addStudent encountered an error:', err);
@@ -19,13 +19,24 @@ studentController.addStudent = (req, res, next) => {
 
 studentController.deleteStudent = (req, res, next) => {
   const { firstName, lastName, classroomID } = req.body;
-  const queryText = `DELETE FROM students WHERE firstName = ${firstName} AND lastName = ${lastName} AND classroomID = ${classroomID}`;
+  const queryText = `DELETE FROM students WHERE first_name = ${firstName} AND last_name = ${lastName} AND classroomID = ${classroomID}`;
   studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
     if(err) {
       console.log('studentController.deleteStudent encountered an error:', err);
     }
   });
   return next();
-}
+};
+
+studentController.addClap = (req, res, next) => {
+  const { firstName, lastName, classroomID, claps } = req.body;
+  const queryText = `UPDATE students SET claps = ${claps + 1} WHERE first_name = ${firstName} AND last_name = ${lastName} AND classroomID = ${classroomID}`;
+  studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
+    if(err) {
+      console.log('studentController.addClap encountered an error:', err);
+    }
+  });
+  return next();
+};
 
 module.exports = studentController;
