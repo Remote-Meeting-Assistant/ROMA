59902fccb533f7e7aa0da6ab56cd7d129992c6a9
Ryan Griggs <griggsspace@gmail.com>
2023-06-05 18:54:21 -0700

    updating studentController

--- server/controllers/StudentController.js
+++ server/controllers/StudentController.js
@@ -6,37 +6,41 @@ const studentController = {};
 // *** ALL QUERIES CURRENTLY ASSUME STUDENT TABLE TO BE LABELED 'students' and CLASSROOM TABLE TO BE LABELED 'classrooms' ***
 
 studentController.addStudent = (req, res, next) => {
-  const { firstName, lastName, classroomID } = req.body;
-  console.log('studentController.addStudent adding:', firstName, lastName, classroomID);
-  const queryText = `INSERT INTO students (first_name, last_name, classroomID, claps) VALUES (${firstName}, ${lastName}, ${classroomID}, 0)`;
-  studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
-    if(err) {
-      console.log('studentController.addStudent encountered an error:', err);
-    }
-  });
-  return next();
+  try {
+    const { firstName, lastName, classroomID } = req.body;
+    console.log('studentController.addStudent adding:', firstName, lastName, classroomID);
+    const queryText = `INSERT INTO students (first_name, last_name, classroomID, claps) VALUES (${firstName}, ${lastName}, ${classroomID}, 0)`;
+    const result = studentDB.query(queryText, [firstName, lastName, classroomID]);
+    return next();
+  }
+  catch (err){ 
+    console.log('studentController.addStudent encountered an error:', err);
+  }
 };
 
 studentController.deleteStudent = (req, res, next) => {
+  try {
   const { firstName, lastName, classroomID } = req.body;
   const queryText = `DELETE FROM students WHERE first_name = ${firstName} AND last_name = ${lastName} AND classroomID = ${classroomID}`;
-  studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
-    if(err) {
-      console.log('studentController.deleteStudent encountered an error:', err);
-    }
-  });
+  studentDB.query(queryText, [firstName, lastName, classroomID]);
+  res.locals.deletedStudent = {firstName: firstName, lastName: lastName, classroomID: classroomID};
   return next();
+}
+catch (err) {
+  console.log('studentController.deleteStudent encountered an error:', err);
+}
 };
 
 studentController.addClap = (req, res, next) => {
-  const { firstName, lastName, classroomID, claps } = req.body;
+  try {
+    const { firstName, lastName, classroomID, claps } = req.body;
   const queryText = `UPDATE students SET claps = ${claps + 1} WHERE first_name = ${firstName} AND last_name = ${lastName} AND classroomID = ${classroomID}`;
-  studentDB.query(queryText, [firstName, lastName, classroomID], (err) => { 
-    if(err) {
-      console.log('studentController.addClap encountered an error:', err);
-    }
-  });
+  studentDB.query(queryText, [firstName, lastName, classroomID]);
   return next();
+}
+catch (err) {
+  console.log('studentController.deleteStudent encountered an error:', err);
+}
 };
 
 module.exports = studentController;
